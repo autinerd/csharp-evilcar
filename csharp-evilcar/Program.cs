54aa0f1949f6504306dba44c7628acf039e24410
autinerd <sidneyjohn23@icloud.com>
2019-03-11 17:43:17 +0100

    Add Json.NET

--- csharp-evilcar/Program.cs
+++ csharp-evilcar/Program.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace csharp_evilcar
+namespace CsharpEvilcar
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			
 		}
 	}
 }
