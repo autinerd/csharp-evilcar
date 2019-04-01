dcb03c13972b317a2ce1541640570c8b1ad4f5ac
NexCastus <johannes.staib@gmail.com>
2019-04-01 13:07:45 +0200

    https://www.youtube.com/watch?v=t2agOmUKin8
    PDF added

--- csharp-evilcar/FleetManager.cs
+++ csharp-evilcar/FleetManager.cs
@@ -8,5 +8,18 @@ namespace CsharpEvilcar
 {
 	class FleetManager
 	{
+		private string _name;
+		private Branch _branch;
+
+		public string name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+		public Branch branch
+		{
+			get { return _branch; }
+			set { _branch = branch; }
+		}
 	}
 }
